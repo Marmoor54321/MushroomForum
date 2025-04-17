7173915c5f2d7d311a7be2d7d96d6665312aac73
Marmoor54321 <89205@student.pb.edu.pl>
2025-04-17 22:52:52 +0200

    Add project files.

---
+++ MushroomForum/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MushroomForum.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
