4970ca672be7fd0614cb7275ddd08104d1bb936a
Anastazja Czech <89315@student.pb.edu.pl>
2025-04-26 21:38:43 +0200

    małe korekty buttonów, widoki post

---
+++ MushroomForum/Migrations/20250426191903_2.cs
@@ -0,0 +1,132 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MushroomForum.Migrations
+{
+    /// <inheritdoc />
+    public partial class _2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Posts_ForumThreads_ForumThreadId",
+                table: "Posts");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "Posts",
+                type: "nvarchar(64)",
+                maxLength: 64,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(64)",
+                oldMaxLength: 64,
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Posts",
+                type: "nvarchar(512)",
+                maxLength: 512,
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(512)",
+                oldMaxLength: 512,
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "CategoryId",
+                table: "ForumThreads",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Categories",
+                columns: table => new
+                {
+                    CategoryId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Categories", x => x.CategoryId);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ForumThreads_CategoryId",
+                table: "ForumThreads",
+                column: "CategoryId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_ForumThreads_Categories_CategoryId",
+                table: "ForumThreads",
+                column: "CategoryId",
+                principalTable: "Categories",
+                principalColumn: "CategoryId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Posts_ForumThreads_ForumThreadId",
+                table: "Posts",
+                column: "ForumThreadId",
+                principalTable: "ForumThreads",
+                principalColumn: "ForumThreadId",
+                onDelete: ReferentialAction.SetNull);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_ForumThreads_Categories_CategoryId",
+                table: "ForumThreads");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Posts_ForumThreads_ForumThreadId",
+                table: "Posts");
+
+            migrationBuilder.DropTable(
+                name: "Categories");
+
+            migrationBuilder.DropIndex(
+                name: "IX_ForumThreads_CategoryId",
+                table: "ForumThreads");
+
+            migrationBuilder.DropColumn(
+                name: "CategoryId",
+                table: "ForumThreads");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Title",
+                table: "Posts",
+                type: "nvarchar(64)",
+                maxLength: 64,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(64)",
+                oldMaxLength: 64);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Posts",
+                type: "nvarchar(512)",
+                maxLength: 512,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(512)",
+                oldMaxLength: 512);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Posts_ForumThreads_ForumThreadId",
+                table: "Posts",
+                column: "ForumThreadId",
+                principalTable: "ForumThreads",
+                principalColumn: "ForumThreadId",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
