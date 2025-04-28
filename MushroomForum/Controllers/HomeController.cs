354a6c821bf1f10575b292570e148fedad15cbcc
Anastazja Czech <89315@student.pb.edu.pl>
2025-04-28 12:19:52 +0200

    Zmiana home page: memy i ciekawostki

--- MushroomForum/Controllers/HomeController.cs
+++ MushroomForum/Controllers/HomeController.cs
@@ -15,6 +15,28 @@ namespace MushroomForum.Controllers
 
         public IActionResult Index()
         {
+            var ciekawostki = new List<string>
+            {
+                "Grzyby to organizmy, które nie s¹ ani roœlinami, ani zwierzêtami!",
+                "W Japonii grzyby shiitake uwa¿ane s¹ za symbol d³ugowiecznoœci.",
+                "Niektóre grzyby maj¹ zdolnoœæ bioluminescencji, œwiec¹ w ciemnoœci!",
+                "Grzyby truj¹ce mog¹ wygl¹daæ podobnie do grzybów jadalnych, wiêc zawsze upewnij siê, ¿e masz pewnoœæ co do ich identyfikacji!"
+            };
+
+            var random = new Random();
+            var losowaCiekawostka = ciekawostki[random.Next(ciekawostki.Count)];
+
+            var memy = new List<string>
+            {
+                "/images/mem1.jpg",
+                "/images/mem2.jpg",
+                "/images/mem3.jpg",
+                "/images/mem4.jpg"
+            };
+
+            var losowyMem = memy[random.Next(memy.Count)];
+            ViewData["Ciekawostka"] = losowaCiekawostka;
+            ViewData["Mem"] = losowyMem;
             return View();
         }
 
@@ -28,5 +50,6 @@ namespace MushroomForum.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
     }
 }
