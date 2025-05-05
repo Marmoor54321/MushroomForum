b4c768ec332e89082167f56c06c5f13f3b03cd1b
Anastazja Czech <89315@student.pb.edu.pl>
2025-05-05 15:51:25 +0200

    można pobierać pdf notatki :D

--- MushroomForum/Controllers/MushroomNotesController.cs
+++ MushroomForum/Controllers/MushroomNotesController.cs
@@ -217,6 +217,7 @@ namespace MushroomForum.Controllers
                                 col.Item().Image(path, ImageScaling.FitWidth);
                         }
                     });
+
                 });
             });
 
