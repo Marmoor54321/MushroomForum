ee3b41c83b2d7eaedac1bef33c019eb19482d2a0
Anastazja Czech <89315@student.pb.edu.pl>
2025-05-05 14:52:58 +0200

    MushrromNotes model update

--- MushroomForum/Migrations/20250505125136_notesurlchange.Designer.cs
+++ MushroomForum/Migrations/20250505125136_notesurlchange.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using MushroomForum.Data;
 
@@ -11,9 +12,11 @@ using MushroomForum.Data;
 namespace MushroomForum.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250505125136_notesurlchange")]
+    partial class notesurlchange
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- MushroomForum/Models/MushroomNotes.cs
+++ MushroomForum/Models/MushroomNotes.cs
@@ -5,7 +5,7 @@
         public int Id { get; set; }
         public string Title { get; set; }
         public string Content { get; set; }
-        public string PhotoUrl { get; set; }
+        public string ? PhotoUrl { get; set; }
         public DateTime CreateDate { get; set; }
     }
 }
