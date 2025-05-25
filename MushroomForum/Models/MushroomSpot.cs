37ca9eafa0f007038eeb76c57e1571b84c49f325
Anastazja Czech <89315@student.pb.edu.pl>
2025-05-25 22:32:02 +0200

    model MushroomSpot - model do mapy przecowującej wiele pinezek z miejscami grzybobrania

---
+++ MushroomForum/Models/MushroomSpot.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace MushroomForum.Models
+{
+    public class MushroomSpot
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; }
+
+        [StringLength(300)]
+        public string Description { get; set; }
+
+        [Range(-90, 90)]
+        public double Latitude { get; set; }
+
+        [Range(-180, 180)]
+        public double Longitude { get; set; }
+
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+    }
+}
