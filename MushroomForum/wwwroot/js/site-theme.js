44b88a9fe038abd81c3a51e0c5355187f5f2d334
Anastazja Czech <89315@student.pb.edu.pl>
2025-06-05 15:34:23 +0200

    uporządkowanie kodu layout

---
+++ MushroomForum/wwwroot/js/site-theme.js
@@ -0,0 +1,29 @@
+﻿// Inicjalizacja Bootstrapowych dropdownów
+var dropdownElementList = [].slice.call(document.querySelectorAll('.dropdown-toggle'));
+var dropdownList = dropdownElementList.map(function (dropdownToggleEl) {
+    return new bootstrap.Dropdown(dropdownToggleEl);
+});
+
+// Funkcja zmieniająca motyw
+function toggleTheme() {
+    const link = document.getElementById('themeStylesheet');
+    const currentHref = link.getAttribute('href');
+
+    if (currentHref.includes('site.css')) {
+        link.setAttribute('href', '/css/site2.css');
+        localStorage.setItem('theme', 'dark');
+    } else {
+        link.setAttribute('href', '/css/site.css');
+        localStorage.setItem('theme', 'light');
+    }
+}
+
+// Po załadowaniu strony ustawiamy zapamiętany motyw
+window.addEventListener('DOMContentLoaded', () => {
+    const savedTheme = localStorage.getItem('theme');
+    const link = document.getElementById('themeStylesheet');
+
+    if (savedTheme === 'dark') {
+        link.setAttribute('href', '/css/site2.css');
+    }
+});
