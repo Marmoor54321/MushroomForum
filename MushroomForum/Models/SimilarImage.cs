94d891681be9855f393884b441aa564fcf5f0568
Marmoor54321 <89205@student.pb.edu.pl>
2025-06-07 20:39:53 +0200

    Więcej informazji w Id grzybów, linki do wikipedii

---
+++ MushroomForum/Models/SimilarImage.cs
@@ -0,0 +1,13 @@
+﻿namespace MushroomForum.Models
+{
+    public class SimilarImage
+    {
+        public string Id { get; set; }
+        public string Url { get; set; }
+        public string UrlSmall { get; set; }
+        public string LicenseName { get; set; }
+        public string LicenseUrl { get; set; }
+        public string Citation { get; set; }
+        public double Similarity { get; set; }
+    }
+}
