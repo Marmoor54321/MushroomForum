a28172601d39563a6b40c0f604ca3e982af8ed53
businiak <kosmo.osmok@wp.pl>
2025-06-08 09:14:40 +0200

    szkielet questów/wyzwań dziennych z migracjami

---
+++ MushroomForum/Migrations/20250608054419_AddDailyQuestTypes.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace MushroomForum.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDailyQuestTypes : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "DailyQuestTypes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    QuestType = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Target = table.Column<int>(type: "int", nullable: false),
+                    DayOfWeek = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DailyQuestTypes", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "DailyQuestTypes",
+                columns: new[] { "Id", "DayOfWeek", "Description", "QuestType", "Target" },
+                values: new object[,]
+                {
+                    { 1, 1, "Polub 3 posty", "LikePosts", 3 },
+                    { 2, 2, "Odpowiedz na 3 posty", "ReplyPosts", 3 },
+                    { 3, 3, "Przeglądaj 5 postów", "ViewPosts", 5 },
+                    { 4, 4, "Polub 3 posty", "LikePosts", 3 },
+                    { 5, 5, "Odpowiedz na 3 posty", "ReplyPosts", 3 },
+                    { 6, 6, "Przeglądaj 5 postów", "ViewPosts", 5 },
+                    { 19, 0, "Rozwiąż quiz", "SolveQuiz", 1 }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DailyQuestTypes");
+        }
+    }
+}
---
+++ MushroomForum/Models/DailyQuestProgress.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+public class DailyQuestProgress
+{
+    public int Id { get; set; }
+    public string UserId { get; set; }
+    public IdentityUser User { get; set; }
+
+    public DateTime Date { get; set; } 
+
+    public string QuestType { get; set; } 
+    public int Progress { get; set; }
+    public bool Completed { get; set; }
+}
